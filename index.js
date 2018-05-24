d6baaf0a0a7ebbd58fc6edad74db49dd70717068
Vladislav Chugunov <vladislav.chugunov@VCHUGUNOV.intexsoft.by>
2018-05-24 12:27:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let http = require("http"),
+	url = require("url");
+
+function start() {
+	http.createServer(function(req, res){
+		var pathname = url.parse(req.url).pathname;
+
+		if (pathname === "/start") {
+			hello();
+		}
+		
+		var body = 
+		'<html>' +
+			'<head>' +
+				'<meta charset=UTF-8" />' +
+			'</head>' +
+			'<body>' +
+				'<form action="/start" method="POST">' +
+					'<input type="submit" value="Click" />' +
+				'</form>' +
+			'</body>' +
+		'</html>';
+		
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write(body);
+		res.end();
+	}).listen(1111);
+	console.log("Server started");
+}
+
+start();
+
+function hello(req, res) {
+	res.writeHead(200, { "Content-Type": "text/html" });
+	res.write("Hello World");
+	res.end();
+}
+
