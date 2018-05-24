0112b424f8059261557ad3460eb54584fd5b2a03
Vladislav Chugunov <vladislav.chugunov@VCHUGUNOV.intexsoft.by>
2018-05-24 13:11:39 +0300

    Optimized code v2.1

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 let http = require("http"),
-	url = require("url");
+url = require("url"),
+port = (process.argv[2] === undefined) ? 1111 : process.argv[2];
 
-start();
-function start() {
-	http.createServer(function(req, res){
-		var pathname = url.parse(req.url).pathname;
+http.createServer(function(req, res){
+	var pathname = url.parse(req.url).pathname;
 
-		if (pathname === "/start") {
-			hello(req, res);
-		} else if (pathname === "" || pathname === "/") {
-				var body = 
-			'<html>' +
-				'<head>' +
-					'<meta charset=UTF-8" />' +
-				'</head>' +
-				'<body>' +
-					'<form action="/start" method="POST">' +
-						'<input type="submit" value="Click" />' +
-					'</form>' +
-				'</body>' +
-			'</html>';
-			
-			res.writeHead(200, { "Content-Type": "text/html" });
-			res.write(body);
-			res.end();
-		}
-		
-		
-	}).listen(1111);
-	console.log("Server started");
-}
+	if (pathname === "/start") {
+		hello(req, res);
+	} else if (pathname === "" || pathname === "/") {
+			var body = 
+		'<html>' +
+			'<head>' +
+				'<meta charset=UTF-8" />' +
+			'</head>' +
+			'<body>' +
+				'<form action="/start" method="POST">' +
+					'<input type="submit" value="Click" />' +
+				'</form>' +
+			'</body>' +
+		'</html>';	
+
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write(body);
+		res.end();
+	}
+}).listen(port);
+console.log("Server started on port: ", port);
+
 
 function hello(req, res) {
 	res.writeHead(200, { "Content-Type": "text/html" });
